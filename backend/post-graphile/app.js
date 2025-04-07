2bdca92a73931d86a464d97c33500373d86e254a
Your Name <you@example.com>
2025-04-07 01:18:09 +0000

    remove plugins

--- backend/post-graphile/app.js
+++ backend/post-graphile/app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Construct the connection string using environment variables
 const DATABASE_APP_URL = `postgres://${process.env.DATABASE_MIGRATE_USER}:${process.env.DATABASE_MIGRATE_PASSWORD}@postgres:5432/${process.env.APPLICATION_DB}`;
 
+// const allowedOriginPlugin = require('./allowedOriginPlugin');
 // Add PostGraphile middleware to Express
 app.use(
   postgraphile(
@@ -22,15 +23,14 @@ app.use(
       simpleCollections: 'only',
       exportGqlSchemaPath: '/app/schema/schema.graphql',
       sortExport: true,
-      appendPlugins: [PgSimplifyInflectorPlugin],
-      cors: true,
+      // appendPlugins: [PgSimplifyInflectorPlugin, allowedOriginPlugin],
+      enableCors: true,
     }
   )
 );
 
 // Uncomment to use the allowed origin plugin
-// const allowedOriginPlugin = require('./allowedOriginPlugin');
-// app.use(allowedOriginPlugin());
+//app.use(allowedOriginPlugin());
 
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {

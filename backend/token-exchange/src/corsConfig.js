1ab13230a249e07fca0bf044aa58b200958f7688
Your Name <you@example.com>
2025-04-14 03:01:28 +0000

    updates

--- backend/token-exchange/src/corsConfig.js
+++ backend/token-exchange/src/corsConfig.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 // Configure CORS for specific origin
 const corsOptions = {
   origin: [
+    'https://simplyserved.app',
     'http://localhost:3000',
     'http://localhost:5000',
     'https://simplyserved.app:5001',
     'http://conan-devbox3.westus3.cloudapp.azure.com:3000',
     'http://conan-devbox3.westus3.cloudapp.azure.com',
-    'https://simplyserved.app'
   ],
   methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
   allowedHeaders: [
--- backend/token-exchange/src/googleAuth.js
+++ backend/token-exchange/src/googleAuth.js
@@ -9,7 +9,8 @@ async function verifyGoogleToken(token) {
   const client = new OAuth2Client();
   const ticket = await client.verifyIdToken({
     idToken: token,
-    audience: "413215377675-cc4qoe522uf7ge33ss1rfp9mkni1lihs.apps.googleusercontent.com",
+    audience: "413215377675-2tdbjabr2noieq1vpi4aubujg6cf2epu.apps.googleusercontent.com",
+    //audience: "413215377675-cc4qoe522uf7ge33ss1rfp9mkni1lihs.apps.googleusercontent.com", // Test Aud
     // Or, if multiple clients access the backend:
     //[WEB_CLIENT_ID_1, WEB_CLIENT_ID_2, WEB_CLIENT_ID_3]
   });
